c735101817c4bb082020f655db902a12180550a4
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2021-03-02 20:16:19 +0530

    Finally chess board can be seen with all the pieces (chessmen, yes I googled it :))

--- main.cpp
+++ main.cpp
@@ -1,35 +1,36 @@
 #include <SFML/Graphics.hpp>
+#include<iostream>
 using namespace sf;
-#define X 700.0f
+// For spacing the board and the pieces
+#define X 702.0f
 #define Y 75.0f
-#define gapX 2.0f
-#define gapY 52.0f
+// Spacing between the pieces
+#define spacing 51
+// Background board color
 #define boardColor Color(150,100,45,255)
+// Paths to all the files
+std::string paths[] {"images\\white-rook.png","images\\white-knight.png","images\\white-bishop.png","images\\white-queen.png","images\\white-king.png","images\\white-pawn.png","images\\black-rook.png","images\\black-knight.png","images\\black-bishop.png","images\\black-queen.png","images\\black-king.png","images\\black-pawn.png"};
+// Board image path
 #define board_path "images\\board.jpg"
-#define black_queen_path "images\\black-queen.png"
-#define white_queen_path "images\\white-queen.png"
-#define black_pawn_path "images\\black-pawn.png"
-#define white_pawn_path "images\\white-pawn.png"
-#define black_king_path "images\\black-king.png"
-#define white_king_path "images\\white-king.png"
-#define black_bishop_path "images\\black-bishop.png"
-#define white_bishop_path "images\\white-bishop.png"
-#define white_rook_path "images\\white-rook.png"
-#define black_rook_path "images\\black-rook.png"
-#define white_knight_path "images\\white-knight.png"
-#define black_knight_path "images\\black-knight.png"
-int boardLayout[8][8] = {{-1,-2,-3,-4,-5,-3,-2,-1},
-                         {-6,-6,-6,-6,-6,-6,-6,-6},
-                         {0,0,0,0,0,0,0,0},
-                         {0,0,0,0,0,0,0,0},
-                         {0,0,0,0,0,0,0,0},
-                         {0,0,0,0,0,0,0,0},
-                         {6,6,6,6,6,6,6,6},
-                         {1,2,3,4,5,3,2,1}};
+// Heart of the program (Starting poistion and most probably updated after every move)
+const int boardLayout[8][8] = {{-1,-2,-3,-4,-5,-3,-2,-1},
+                               {-6,-6,-6,-6,-6,-6,-6,-6},
+                               {0,0,0,0,0,0,0,0},
+                               {0,0,0,0,0,0,0,0},
+                               {0,0,0,0,0,0,0,0},
+                               {0,0,0,0,0,0,0,0},
+                               {6,6,6,6,6,6,6,6},
+                               {1,2,3,4,5,3,2,1}};
+// Loading the textures in the texture array
+Texture pieces_text[12];
+// Loading the sprites in the sprite array
+Sprite pieces[12];
 int main()
 {
     // Creating a window for containing all the elements
     RenderWindow window(VideoMode(1200, 600), "Chess");
+    // Limiting the framerate to 60 to decrease the load from GPU
+    window.setFramerateLimit(60);
 
     // Creating a texture and assigning an image to it
     Texture text;
@@ -42,57 +43,13 @@ int main()
     chessBoard.setScale(0.4f,0.4f);
     chessBoard.setPosition({700.0f,75.0f});
 
+    // Loading the textures from the files in the "path" array
+    for(int i = 0 ; i < 12 ; i++)
+        pieces_text[i].loadFromFile(paths[i]);
 
-    // Creating all the sprites
-    text.loadFromFile(white_queen_path);
-    Sprite whiteQueen;
-    whiteQueen.setTexture(text);
-
-    text.loadFromFile(black_queen_path);
-    Sprite blackQueen;
-    whiteQueen.setTexture(text);
-
-    text.loadFromFile(black_bishop_path);
-    Sprite blackBishop;
-    blackBishop.setTexture(text);
-
-    text.loadFromFile(white_bishop_path);
-    Sprite whiteBishop;
-    whiteBishop.setTexture(text);
-
-    text.loadFromFile(black_knight_path);
-    Sprite blackKnight;
-    blackKnight.setTexture(text);
-
-    text.loadFromFile(white_knight_path);
-    Sprite whiteKnight;
-    whiteKnight.setTexture(text);
-
-    text.loadFromFile(black_rook_path);
-    Sprite blackRook;
-    blackRook.setTexture(text);
-
-    text.loadFromFile(white_rook_path);
-    Sprite whiteRook;
-    whiteRook.setTexture(text);
-
-    text.loadFromFile(black_king_path);
-    Sprite blackKing;
-    blackKing.setTexture(text);
-
-    text.loadFromFile(white_king_path);
-    Sprite whiteKing;
-    whiteKing.setTexture(text);
-
-    text.loadFromFile(black_pawn_path);
-    Sprite blackPawn;
-    blackPawn.setTexture(text);
-
-    text.loadFromFile(white_pawn_path);
-    Sprite whitePawn;
-    whitePawn.setTexture(text);
-    // All sprites created
-
+    // Setting the textures to different spirites
+    for(int i = 0 ; i < 12 ; i++)
+        pieces[i].setTexture(pieces_text[i]);
 
     // Main game loop
     while (window.isOpen())
@@ -109,41 +66,26 @@ int main()
                 }
             }
         }
+        // Clearing with the board color so that it looks good :)
         window.clear(boardColor);
+        // Drawing the chess board
+        window.draw(chessBoard);
+        // Main drawing loop that should be executed every single frame
         for(int i = 0 ; i < 8 ; i++) {
             for(int j = 0 ; j < 8 ; j++) {
-                switch (boardLayout[i][j])
-                {
-                case -6:    window.draw(blackPawn);
-                            break;
-                case -5:    window.draw(blackKing);
-                            break;
-                case -4:    window.draw(blackQueen);
-                            break;
-                case -3:    window.draw(blackBishop);
-                            break;
-                case -2:    window.draw(blackKnight);
-                            break;
-                case -1:    window.draw(blackRook);
-                            break;
-                case 0:
-                            break;
-                case 1:     window.draw(whiteRook);
-                            break;
-                case 2:     window.draw(whiteKnight);
-                            break;
-                case 3:     window.draw(whiteBishop);
-                            break;
-                case 4:     window.draw(whiteKing);
-                            break;
-                case 5:     window.draw(whiteQueen);
-                            break;
-                case 6:     window.draw(whitePawn);
-                            break;
+                if(boardLayout[i][j] != 0) {
+                    if(boardLayout[i][j] > 0) {
+                        pieces[boardLayout[i][j] - 1].setPosition({X+j*spacing,Y+i*spacing});
+                        window.draw(pieces[boardLayout[i][j] - 1]);
+                    }
+                    else {
+                        pieces[std::abs(boardLayout[i][j]) + 5].setPosition({X+j*spacing,Y+i*spacing});
+                        window.draw(pieces[std::abs(boardLayout[i][j]) + 5]);
+                    }
                 }
             }
         }
-        window.draw(chessBoard);
+        // Displaying all the sprites that were drawn on the window
         window.display();
     }
 
