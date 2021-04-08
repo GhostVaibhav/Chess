405f9f9dfea7ceadd782e328fa748688cfb4510b
Vaibhav Sharma <48472541+GhostVaibhav@users.noreply.github.com>
2021-04-08 23:38:47 +0530

    Create buildWorkflow.cpp

---
+++ buildWorkflow.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(int argc,char* argv[]) {
+    for(int i = 1 ; i < argc ; i++) {
+        string fileName = argv[1];
+        string cmd;
+        cmd = "g++ -Isrc/include -c " + fileName + ".cpp";
+        system(cmd.c_str());
+        cmd = "g++ " + fileName + ".o -o " + fileName + " -Lsrc/lib -lsfml-audio -lsfml-graphics -lsfml-window -lsfml-system icon.res";
+        system(cmd.c_str());
+        cmd = "del " + fileName + ".o";
+        system(cmd.c_str());
+        cmd = "move " + fileName + ".exe Builds > NUL";
+        system(cmd.c_str());
+        system("cls");
+    }
+    return 0;
+}
