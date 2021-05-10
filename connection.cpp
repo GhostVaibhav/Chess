2fdde29dd64a96084412cfb9d32f3ffe5cd27172
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2021-05-10 23:28:42 +0530

    Tried connecting with "stockfish" engine using pipes

--- connection.cpp
+++ connection.cpp
@@ -2,14 +2,14 @@
 #include <tchar.h>
 #include <stdio.h> 
 #include <bits/stdc++.h>
-
-#define bufferSize 4096
+#define bufferSize 2048
 
 HANDLE engine_read_out = NULL;
 HANDLE engine_read_in = NULL;
 HANDLE engine_write_in = NULL;
 HANDLE engine_write_out = NULL;
 HANDLE process;
+BYTE buffer[bufferSize];
 
 bool connectEngine(std::string PATH) {
     TCHAR l[] = TEXT("Engine/stockfish_13.exe");
@@ -22,32 +22,46 @@ bool connectEngine(std::string PATH) {
     sa.bInheritHandle = TRUE;
     sa.lpSecurityDescriptor = NULL;
     sa.nLength = sizeof(SECURITY_ATTRIBUTES);
-    CreatePipe(&engine_read_in,&engine_read_out,&sa,0);
-    CreatePipe(&engine_write_in,&engine_write_out,&sa,0);
-    s.hStdOutput = engine_write_out;
-    s.hStdError = engine_write_out;
+    CreatePipe(&engine_read_in,&engine_write_in,&sa,0);
+    CreatePipe(&engine_read_out,&engine_write_out,&sa,0);
+    s.dwFlags = STARTF_USESTDHANDLES;
+    s.hStdOutput = engine_read_out;
+    s.hStdError = engine_read_out;
     s.hStdInput = engine_read_in;
     connected = CreateProcess(NULL,l,NULL,NULL,TRUE,0x00000010,NULL,NULL,&s,&p);
-    process = p.hProcess;
     if(!connected) {
         std::cout << "Not connected";
         return false;
     }
+    process = p.hProcess;
     return true;
 }
 void demoWrite() {
+    std::string demo;
     std::cout << "Inside Demo" << std::endl;
-    DWORD read;
-    char BUFFER[150];
-    std::string pos = "position startpos moves e2e4\n go depth 20\n";
-    ReadFile(engine_write_out,BUFFER,150,&read,NULL);
-    WriteFile(engine_read_in,pos.c_str(),sizeof(pos),&read,NULL);
+    DWORD write, read, available;
+    std::string pos = "position startpos moves e2e4\ngo depth 20\n";
+    std::cout << WriteFile(engine_write_in,pos.c_str(),pos.length(),&write,NULL) << std::endl;
+    std::cout << PeekNamedPipe(engine_read_out,&buffer,bufferSize,&read,&available,NULL) << std::endl;
+    do {
+        ZeroMemory(buffer,bufferSize);
+        std::cout << "In loop" << std::endl;
+        if(!ReadFile(engine_write_out,buffer,bufferSize,&read,NULL) || !read)
+            break;
+        buffer[read] = 0;
+        demo += (char *)(buffer);
+    }while(read >= bufferSize);
+    std::cout << demo << std::endl;
 }
 std::string bestmove(std::string move) {
     std::string pos = "position startpos moves " + move + "\ngo depth 20\n";
     WriteFile(engine_write_out,pos.c_str(),sizeof(pos),NULL,NULL);
 }
 bool disconnectEngine() {
+    CloseHandle(engine_read_in);
+    CloseHandle(engine_read_out);
+    CloseHandle(engine_write_in);
+    CloseHandle(engine_write_out);
     if(TerminateProcess(process,0) && CloseHandle(process))
         return true;
     return false;
